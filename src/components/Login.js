02b2ead0ee98b07ad558cf0ec369dbe88d22c913
Matt <cunning.distraction@googlemail.com>
2022-03-30 18:41:13 +0100

    add login textboxes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,27 @@
 const Login = () => {
-  return <>Log in plz</>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <form>
+        <label>
+          Username: <input type="text" placeholder="Username" />
+        </label>
+        <br />
+        <label>
+          Password: <input type="password" placeholder="Password" />
+        </label>
+        <br />
+        <button>Log in</button>
+      </form>
+      <p>
+        For demo purposes log in with: <br />
+        username: jessjelly
+        <br />
+        password: admin
+      </p>
+    </>
+  );
 };
 export default Login;
