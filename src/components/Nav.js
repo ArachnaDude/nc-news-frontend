3dbae3d717b8203092908ed6cbea9657fc083dc5
Matt <cunning.distraction@googlemail.com>
2022-04-14 18:01:51 +0100

    added dropdown functionality to nav for topic select

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+  const navigate = useNavigate();
+  const [filter, setFilter] = useState("all");
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+    navigate(`/articles/topics/${event.target.value}`);
+  };
+
   return (
     <nav className="navBar">
-      Topics: <Link to="/articles/topics/football">Football</Link> |{" "}
-      <Link to="/articles/topics/cooking">Cooking</Link> |{" "}
-      <Link to="/articles/topics/coding">Coding</Link>
+      <select value={filter} onChange={handleChange}>
+        <option disabled value="all">
+          All
+        </option>
+        <option value="coding">Coding</option>
+        <option value="football">Football</option>
+        <option value="cooking">Cooking</option>
+      </select>
     </nav>
   );
 };
