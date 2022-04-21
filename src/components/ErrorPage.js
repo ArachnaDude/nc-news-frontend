a3d9bd8a147c706c5794855dc73710ee4c902973
Matt <cunning.distraction@googlemail.com>
2022-04-21 23:46:26 +0100

    add 404 page not found error as default option

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,7 +1,11 @@
-const ErrorPage = (props) => {
-  console.log(props, "props");
-
-  return <h1>error page</h1>;
+const ErrorPage = ({ status, message }) => {
+  return (
+    <>
+      <p>
+        <strong>{status || 404} error:</strong> {message || "page not found"}
+      </p>
+    </>
+  );
 };
 
 export default ErrorPage;
