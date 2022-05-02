94dec14522d5944fe46311a59b8ea05ec899252e
Matt <cunning.distraction@googlemail.com>
2022-05-02 18:35:56 +0100

    allow one upvote/downvote per page load

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -10,14 +10,15 @@ const Comment = ({ comment, comments, setComments }) => {
 
   const [localVote, setLocalVote] = useState(0);
 
+  const [disableUpvote, setDisableUpvote] = useState(false);
+  const [disableDownvote, setDisableDownvote] = useState(false);
+
   const handleDelete = () => {
-    const filteredComments = comments.filter((remark) => {
-      return remark.comment_id !== comment.comment_id;
+    const filteredComments = comments.filter((c) => {
+      return c.comment_id !== comment.comment_id;
     });
     setComments(filteredComments);
-    deleteComment(comment.comment_id).then(() => {
-      console.log(`you deleted comment number ${comment.comment_id}`);
-    });
+    deleteComment(comment.comment_id);
   };
 
   const handleVotes = (clickDirection) => {
@@ -26,6 +27,22 @@ const Comment = ({ comment, comments, setComments }) => {
     });
     patchCommentVotes(comment.comment_id, clickDirection);
   };
+
+  /*
+
+  if upvote -> disable upvote button && patch vote by +1
+
+  if downvote -> disable downvote button && patch vote by -1
+
+  if downvote AFTER upvote:
+  re-enable upvote
+  patch downvote by -2
+
+  if upvote AFTER downvote:
+  re-enable downvote
+  patch upvote by +2
+
+  */
 
   return (
     <li className="commentCard" key={comment.comment_id}>
@@ -42,15 +59,27 @@ const Comment = ({ comment, comments, setComments }) => {
       loggedInUser.username === comment.author ? null : (
         <>
           <button
+            disabled={disableUpvote}
             onClick={() => {
               handleVotes(1);
+              setDisableUpvote(true);
+              if (disableDownvote) {
+                handleVotes(1);
+                setDisableDownvote(false);
+              }
             }}
           >
             upvote this comment
           </button>
           <button
+            disabled={disableDownvote}
             onClick={() => {
               handleVotes(-1);
+              setDisableDownvote(true);
+              if (disableUpvote) {
+                handleVotes(-1);
+                setDisableUpvote(false);
+              }
             }}
           >
             downvote this comment
@@ -62,7 +91,6 @@ const Comment = ({ comment, comments, setComments }) => {
         <button
           onClick={() => {
             handleDelete();
-            // deleteComment(comment.comment_id);
           }}
         >
           delete comment
