470c7e31f8086b09cbe5b122bbd61511b48fe92c
Matt <cunning.distraction@googlemail.com>
2022-05-03 15:59:51 +0100

    add back button to error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
 const ErrorPage = ({ status, message }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
-    <p>
-      <strong>{status || 404} error:</strong> {message || "page not found"}
-    </p>
+    <>
+      <p>
+        <strong>{status || 404} error:</strong> {message || "page not found"}
+      </p>
+      <button onClick={handleClick}>Click here to go back</button>
+    </>
   );
 };
 
