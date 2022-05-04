a2465b661f915cad068612663940091c60230ea7
Matt <cunning.distraction@googlemail.com>
2022-05-04 21:59:11 +0100

    add coming soon message

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+const ComingSoon = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="errorMessage">
+      <p>
+        This feature is still in development. <br /> Check back soon!
+      </p>
+      <button onClick={handleClick}>Click here to go back</button>
+    </div>
+  );
+};
+
+export default ComingSoon;
