d296f23def2f2f73437fed8fa5481600925b8fbf
Matt <cunning.distraction@googlemail.com>
2022-05-04 21:07:43 +0100

    style change

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getUserProfile } from "../utils/api";
 import ErrorPage from "./ErrorPage";
 
 const User = () => {
+  const navigate = useNavigate();
   const { username } = useParams();
   const [currentUser, setCurrentUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const [error, setError] = useState(null);
+
+  const handleClick = () => {
+    navigate(-1);
+  };
 
   useEffect(() => {
     getUserProfile(username)
@@ -16,14 +22,23 @@ const User = () => {
       })
       .catch(({ response }) => {
         setError({ status: response.status, message: response.data.msg });
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [username]);
 
   if (error) {
     return <ErrorPage status={error.status} message={error.message} />;
   }
-  return (
+  return isLoading ? (
+    <p className="loadingMessage">
+      Loading user profile{console.log("loading user profile")}
+    </p>
+  ) : (
     <>
+      <button onClick={handleClick}>Click here to go back</button>
+
       <img
         className="userProfile__picture"
         src={currentUser.avatar_url}
@@ -32,7 +47,9 @@ const User = () => {
       <h2 className="userProfile__username">
         {currentUser.username}'s Profile
       </h2>
-      <p className="userProfile__name">name: {currentUser.name}</p>
+      <p className="userProfile__name">
+        <strong>Name:</strong> {currentUser.name}
+      </p>
 
       <p>list of articles by {currentUser.username} </p>
       <p>list of comments by {currentUser.username}</p>
